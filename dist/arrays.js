3dc76854ef2a0b45eedaebd99a15ae6bdcb08a4c
Saurab Prasai <saurablovetocode@gmail.com>
2024-01-12 20:03:32 +0545

    array in ts

---
+++ dist/arrays.js
@@ -0,0 +1,8 @@
+"use strict";
+const names = ["Sudip", "Ram"];
+names.push("Saurab");
+console.log(names);
+// names.push(3) gives error
+const numbers = [1, 2, 4, 5];
+const num1 = numbers[0];
+console.log(num1);
