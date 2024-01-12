9ec10e56bbdd437ed5b795bec202f374b57ee231
Saurab Prasai <saurablovetocode@gmail.com>
2024-01-12 19:31:39 +0545

    types in ts

---
+++ dist/types.js
@@ -0,0 +1,10 @@
+"use strict";
+let num = 5;
+// num="5"  throws error
+let fName = "Saurab"; //implicitly defined the type
+let gender = true;
+let u;
+u = 5;
+u = "saurab";
+u = true;
+console.log(u);
---
+++ src/types.ts
@@ -0,0 +1,16 @@
+
+let num:number=5;
+
+// num="5"  throws error
+
+let fName="Saurab" //implicitly defined the type
+
+let gender:boolean=true;
+
+let u:any; //disables the typeChecking
+u=5;
+u="saurab"
+u=true;
+
+
+
