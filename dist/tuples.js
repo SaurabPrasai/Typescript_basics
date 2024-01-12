511e1c824040799a1f45d6af190a71b1c142e8a6
Saurab Prasai <saurablovetocode@gmail.com>
2024-01-12 20:20:54 +0545

    Tuples in ts

---
+++ dist/tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+let tuple;
+tuple = [1, "Saurab", true];
+console.log(tuple);
+// tuple=["Saurab",2,true] throws error
+tuple.push(true, "Sa");
+console.log(tuple);
---
+++ src/tuples.ts
@@ -0,0 +1,8 @@
+let tuple:[number,string,boolean];
+
+tuple=[1,"Saurab",true];
+
+console.log(tuple);
+
+// tuple=["Saurab",2,true] throws error
+
